6b877c017bbb9593d28ffb1bc0ea73accccee3bc
root <root@ip-172-31-27-27.ec2.internal>
2026-06-23 06:22:41 +0000

    calculator

---
+++ cal.py
@@ -0,0 +1,8 @@
+num1=(input("enter first number"))
+num2=(input("enter second number"))
+print=("Addition is:",num1+num2)
+print=("Subtraction is:",num1-num2)
+print=("Multiplication  is:",num1*num2)
+print=("Division is:",num1/num2)
+
+
