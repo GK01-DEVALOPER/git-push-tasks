0e9d1b6e6483c07efffe3840135ce6746f203244
GK01-DEVALOPER <devopsc478@gmail.com>
2026-06-23 12:18:54 +0530

    new cal.py

--- cal.py
+++ cal.py
@@ -1,8 +1,8 @@
-num1=(input("enter first number"))
-num2=(input("enter second number"))
-print=("Addition is:",num1+num2)
-print=("Subtraction is:",num1-num2)
-print=("Multiplication  is:",num1*num2)
-print=("Division is:",num1/num2)
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+print("Addition is:",num1 + num2)
+print("Subtraction is:",num1 - num2)
+print("Multiplication  is:",num1 * num2)
+print("Division is:",num1 / num2)
 
 
