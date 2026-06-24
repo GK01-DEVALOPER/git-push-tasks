7e1caf412cf48c1453769601b1ab167ac6facf21
kotesh <koteshjc0@gmail.com>
2026-06-24 06:35:07 +0000

    student infor

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+	public static void main(String[] args){
+		int rollnumber = 4045;
+		String sname = "Kotesh";
+		String course = "ELECTRONICS AND COMMUNICATION ENGINEERING";
+		System.out.println("My Name is:" +sname);
+		System.out.println("My course is:" +course);
+		System.out.println("My Roll Number is:" +rollnumber);
+	}
+}
+
